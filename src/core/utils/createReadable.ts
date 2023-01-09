5d4001426749aff9e6b61d6332d7d7f1efb98414
Fierflame <git@fierflame.com>
2023-01-09 22:11:23 +0800

    新增：实现 createReadable 函数，为后期用 ReadableStream 代替部分功能做准备

---
+++ src/core/utils/createReadable.ts
@@ -0,0 +1,35 @@
+import { WriteType } from '../types';
+import str2bin from './str2bin';
+
+
+async function *toAsyncIterable(
+	chunk?: WriteType
+): AsyncGenerator<Uint8Array> {
+	if (!chunk) { return; }
+	if (typeof chunk === 'string') { return yield str2bin(chunk); }
+	if (typeof chunk !== 'object') { return; }
+	if (chunk instanceof ArrayBuffer) { return yield str2bin(chunk); }
+	if (ArrayBuffer.isView(chunk)) { return yield str2bin(chunk); }
+	if (!(Symbol.asyncIterator in chunk || Symbol.iterator in chunk)) { return; }
+	for await (const data of chunk) {
+		yield *toAsyncIterable(data);
+	}
+}
+
+export default function createReadable(v: any) {
+	const iterator = toAsyncIterable(v);
+	return new ReadableStream({
+		async pull(controller) {
+			const { value, done } = await iterator.next();
+			if (done) {
+			  controller.close();
+			} else {
+			  controller.enqueue(value);
+			}
+		},
+		cancel(reason) {
+			iterator.throw(reason);
+		},
+		type: 'bytes',
+	});
+}
