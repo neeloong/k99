af4ebd0b6820a79a4ac89e5827d466216318a20c
Fierflame <git@fierflame.com>
2023-01-14 23:25:53 +0800

    新增：将 run 导出

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,7 +1,8 @@
 export * from './types';
 
-export { default as Router  } from './Router';
-export { default as ApiRouter  } from './ApiRouter';
-export { default as Plugin  } from './Plugin';
-export { default as App     } from './App';
-export { default as service } from './service';
+export { default as run       } from './run';
+export { default as Router    } from './Router';
+export { default as ApiRouter } from './ApiRouter';
+export { default as Plugin    } from './Plugin';
+export { default as App       } from './App';
+export { default as service   } from './service';
