2948f09c623a308fa100118361ecbee4da08eb37
Fierflame <git@fierflame.com>
2023-01-21 23:21:57 +0800

    环境：更新依赖及打包配置等

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,18 +1,17 @@
 import dts from 'rollup-plugin-dts';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import babel from '@rollup/plugin-babel';
 import replace from '@rollup/plugin-replace';
 import resolve from '@rollup/plugin-node-resolve';
-import fsFn from 'fs';
-import fsPromises from 'fs/promises';
-import info from './package.json';
+import fsPromises from 'node:fs/promises';
+const info = JSON.parse(await fsPromises.readFile('./package.json', 'utf-8'))
 const {
 	name, description, version, engines, dependencies, keywords,
 	author, license, homepage, repository, bugs,
 } = info;
-try { fsFn.rmSync('build', { recursive: true }); } catch {}
-fsFn.mkdirSync('build', {recursive: true});
-fsPromises.writeFile('build/package.json', JSON.stringify({
+await fsPromises.rm('build', { recursive: true }).catch(() =>{})
+await fsPromises.mkdir('build', {recursive: true});
+await fsPromises.writeFile('build/package.json', JSON.stringify({
 	name, description, version, engines, dependencies, keywords,
 	type: 'module', main: 'index.mjs',
 	bin: {k99: 'cli.mjs', 'k99-start': 'starter.mjs'},
@@ -20,7 +19,14 @@ fsPromises.writeFile('build/package.json', JSON.stringify({
 	author, license, homepage, repository, bugs,
 }, null, 2));
 
-const external = [...Object.keys(dependencies), 'k99', 'k99/node', 'k99/cli'];
+const external = [
+	...Object.keys(dependencies),
+	'k99',
+	'k99/node',
+	'k99/cli',
+	'node:http',
+	'node:http2',
+];
 
 const bYear = 2019;
 const year = new Date().getFullYear();
@@ -46,7 +52,7 @@ const nodePackage = JSON.stringify({
 }, null, 2);
 
 
-function plugins(b) {
+function plugins() {
 	const plugins = [
 		resolve({ extensions: [ '.ts' ]}),
 		babel({
@@ -56,13 +62,12 @@ function plugins(b) {
 		}),
 		replace({preventAssignment: true, values: { __VERSION__: version }}),
 	];
-	if (b) { plugins.push(terser()); }
 	return plugins;
 }
 
-function createBaseItem(id) {
-	fsFn.mkdirSync(`build/${ id }`, {recursive: true});
-	fsPromises.writeFile(`build/${ id }/package.json`, nodePackage);
+async function createBaseItem(id) {
+	await fsPromises.mkdir(`build/${ id }`, {recursive: true}).catch(() => {});
+	await fsPromises.writeFile(`build/${ id }/package.json`, nodePackage);
 	const input = `src/${ id }/index.ts`;
 	return [{ input, external, plugins: plugins(), output: [
 		{ banner, file: `build/${ id }/index.mjs`, format: 'esm' },
@@ -70,29 +75,28 @@ function createBaseItem(id) {
 		{ format: 'esm', banner, file: `build/${ id }/index.d.ts` },
 	] }];
 }
-function createBrowserItem(id, name = 'k99') {
+async function createBrowserItem(id, name = 'k99') {
 	if (id) {
-		fsFn.mkdirSync(`build/${ id }`, {recursive: true});
-		fsPromises.writeFile(`build/${ id }/package.json`, browserPackage);
+		await fsPromises.mkdir(`build/${ id }`, {recursive: true}).catch(() => {});
+		await fsPromises.writeFile(`build/${ id }/package.json`, browserPackage);
 	}
 	const input = `src/${ id || 'core' }/index.ts`;
 	const output = `build/${ id ? `${ id }/` : '' }index`;
 	return [ { input, external, plugins: plugins(), output: [
 		{ format: 'esm', banner, file: `${ output }.mjs` },
 		{ format: 'umd', banner, file: `${ output }.js`, exports: 'named', name },
-	] }, { input, external, plugins: plugins(1), output: [
-		{ format: 'esm', banner, file: `${ output }.min.mjs` },
-		{ format: 'umd', banner, file: `${ output }.min.js`, exports: 'named', name },
+		{ format: 'esm', banner, file: `${ output }.min.mjs`, plugins: [terser()] },
+		{ format: 'umd', banner, file: `${ output }.min.js`, plugins: [terser()], exports: 'named', name },
 	] }, { input, external, plugins: [ dts() ], output: [
 		{ format: 'esm', banner, file: `${ output }.d.ts` },
 	] } ];
 }
 export default [
-	...createBrowserItem(),
-	...createBrowserItem('browser', 'k99Browser'),
-	...createBrowserItem('services', 'k99Services'),
-	...createBaseItem('node'),
-	...createBaseItem('cli'),
+	...await createBrowserItem(),
+	...await createBrowserItem('browser', 'k99Browser'),
+	...await createBrowserItem('services', 'k99Services'),
+	...await createBaseItem('node'),
+	...await createBaseItem('cli'),
 	{ input: 'src/cli/cli.ts', external, plugins: plugins(), output: [
 		{ format: 'esm', banner: `#!/usr/bin/env node\n${ banner }`,  file: 'build/cli.mjs'  },
 	] }, { input: 'src/starter.ts', external, plugins: plugins(), output: [
